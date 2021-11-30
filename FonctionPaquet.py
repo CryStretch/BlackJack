4075bf0447e42d1639fb3c24f449b55fe5c7c92b
CryStretch <93593204+CryStretch@users.noreply.github.com>
2021-11-30 19:22:11 +0100

    Add files via upload

---
+++ FonctionPaquet.py
@@ -0,0 +1,10 @@
+from random import *
+def paquet():
+    figure=["2","3","4","5","6","7","8","9","10","valet", "dame", "roi", "as"]
+    paquet=[]
+    figure2=[" de carreau"," de coeur"," de trefle"," de pique"]
+    for e in (figure2):
+        for i in (figure):
+            fig=i+e
+            paquet.append(fig)
+    return paquet
